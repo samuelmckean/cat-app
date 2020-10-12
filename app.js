b3bb5ddce2d8b16e7ff4dc646e32b07a8cd0170c
Samuel McKean <samuel.mckean@gmail.com>
2020-10-12 17:37:08 -0500

    Got API request working

--- app.js
+++ app.js
@@ -1,13 +1,36 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const https = require('https');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const { type } = require('os');
+const port = 3000;
 
-app.use(express.static('public'))
+app.use(bodyParser.json());
+
+// use as x-api-key header when making requests to api
+const apiKey = '9e97415a-c1b8-40ff-bd3a-3f48d2ad16a4'
+const baseUrl = 'https://api.thecatapi.com/v1'
+
+// make request to TheCatAPI for the breeds data
+let breeds;
+const apiRequest = https.get(baseUrl + '/breeds', {headers: {'x-api-key': apiKey}}, (apiResponse) => {
+  let chunks = [];
+  apiResponse.on('data', (chunk) => {
+    chunks.push(chunk);
+  });
+
+  apiResponse.on('end', () => {
+    let body = Buffer.concat(chunks);
+    breeds = JSON.parse(body.toString());
+  });
+});
+apiRequest.end();
 
 app.get('/', (req, res) => {
-  res.sendFile('index.html')
-})
+  console.log(breeds[0].name);
+  res.json('meow');
+});
 
 app.listen(port, () => {
-  console.log(`Cat app listening at http://localhost:${port}`)
-})
+  console.log(`Cat app listening at http://localhost:${port}`);
+});
---
+++ public/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const e = <h1>Meow</h1>
+
+ReactDOM.render(
+  e,
+  document.getElementById('root')
+)
